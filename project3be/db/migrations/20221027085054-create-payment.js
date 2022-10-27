b222f36be546d69457e0919f3c962bdfe287ce02
manfredel <manfrede.lee@gmail.com>
2022-10-27 16:58:26 +0800

    payment setup

---
+++ project3be/db/migrations/20221027085054-create-payment.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('payments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.UUID
+      },
+      cart_id: {
+        type: Sequelize.INTEGER
+      },
+      cart_value: {
+        type: Sequelize.INTEGER
+      },
+      instalment_period: {
+        type: Sequelize.INTEGER
+      },
+      full_payment: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('payments');
+  }
+};
